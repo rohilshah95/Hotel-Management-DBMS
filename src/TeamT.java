aece04ed250f0e1730d0352f6ac9831bdbfc4ad8
rcoutin <rcoutin@ncsu.edu>
2018-04-13 19:52:49 -0400

    Removing redundant code

--- src/TeamT.java
+++ src/TeamT.java
@@ -31,30 +31,6 @@ public class TeamT {
 				System.out.println("\n ENTER CORRE" + "CT CHOICE! \n");
 			}
 		}
-		
-//		while (true) {
-//			switch (selectUser()) {
-//			case 1:
-//				admin();
-//				break;
-//			case 2:
-//				manager();
-//				break;
-//			case 3:
-//				frontDesk();
-//				break;
-//			case 4:
-//				serviceStaff();
-//				break;
-//			case 5:
-//				System.out.println("Exiting!");
-//				System.exit(0);
-//				break;
-//			default:
-//				System.out.println("\n ENTER CORRE" + "CT CHOICE! \n");
-//			}
-//		}
-	}
 	
 	public static void reports(int user) {
 		// TODO Auto-generated method stub
@@ -86,34 +62,6 @@ public class TeamT {
 		System.out.println("Enter the number of your choice:");
 		user = Integer.parseInt(readInput());
 		return user;
-	}
-
-	public static int selectUser() {
-		int user = 0;
-		System.out.println("\n\n******************************************************************\n\n");
-		// Enter user choice
-		System.out.println("Which user would you like to login as?");
-		System.out.println("1. Admin \n" + "2. Hotel Manager \n" + "3. Front Desk Representative \n" + "4. Service Staff \n"
-						+ "5. Exit");
-		System.out.println("Enter the number of your choice:");
-		user = Integer.parseInt(readInput());
-		return user;
-	}
-
-	public static void admin() {
-		System.out.println("Admin");
-	}
-
-	public static void manager() {
-		System.out.println("Manager");
-	}
-
-	public static void frontDesk() {
-		System.out.println("Front Desk Representative");
-	}
-
-	public static void serviceStaff() {
-		System.out.println("Service Staff");
 	}
 
 	public static String readInput() {
