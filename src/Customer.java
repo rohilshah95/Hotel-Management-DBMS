c6bc5a7f99e52c5deccd0312144ed8519e994fc1
Rohil Shah <rshah8@ncsu.edu>
2018-04-13 19:45:14 -0400

    add db connections everywhere

--- src/Customer.java
+++ src/Customer.java
@@ -27,6 +27,9 @@ public class Customer {
 	public void updateCustomer(String id, String name, String dob, String phoneNumber, String email, String ssn,
 			String address, String hasHotelCard) {
 		try {
+			Connection conn = DBConnection.getConnection();
+		    Statement stmt = conn.createStatement();
+		    ResultSet rs = null;
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
@@ -35,6 +38,9 @@ public class Customer {
 
 	public void deleteCustomer(String id) {
 		try {
+			Connection conn = DBConnection.getConnection();
+		    Statement stmt = conn.createStatement();
+		    ResultSet rs = null;
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
@@ -43,6 +49,9 @@ public class Customer {
 
 	public void assignRoom(int customerId, int hotelId, int roomId) {
 		try {
+			Connection conn = DBConnection.getConnection();
+		    Statement stmt = conn.createStatement();
+		    ResultSet rs = null;
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
--- src/Service.java
+++ src/Service.java
@@ -1,42 +1,41 @@
 package src;
 
-public class Service
-{
-  private void createService(String name, String cost)
-  {
-    try
-    {
-      //query
-    }
-    catch(Exception e)
-    {
-      System.out.println(e);
-    }
-  }
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-  private void deleteService(String id)
-  {
-    try
-    {
-      //query
-    }
-    catch(Exception e)
-    {
-      System.out.println(e);
-    }
-  }
+public class Service {
+	private void createService(String name, String cost) {
+		try {
+			Connection conn = DBConnection.getConnection();
+		    Statement stmt = conn.createStatement();
+		    ResultSet rs = null;
+			// query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
-  private void updateService(String id, String name, String cost)
-  {
-    try
-    {
-      //query
-    }
-    catch(Exception e)
-    {
-      System.out.println(e);
-    }
-  }
+	private void deleteService(String id) {
+		try {
+			Connection conn = DBConnection.getConnection();
+		    Statement stmt = conn.createStatement();
+		    ResultSet rs = null;
+			// query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
+	private void updateService(String id, String name, String cost) {
+		try {
+			Connection conn = DBConnection.getConnection();
+		    Statement stmt = conn.createStatement();
+		    ResultSet rs = null;
+			// query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
 }
