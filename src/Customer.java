1c99e1ece1b155f57ead86e7b3ae3ef34137b368
Uddhav Bhosle <ubhosle@ncsu.edu>
2018-04-13 19:12:55 -0400

    sex

--- src/Customer.java
+++ src/Customer.java
@@ -1,62 +1,47 @@
 package src;
 
-public class Customer
-{
-  // int id;
-  // String name;
-  // String dob;
-  // String phoneNumber;
-  // String email;
-  // String ssn;
-  // String address;
-  // byte hasHotelCard;
+public class Customer {
+	// int id;
+	// String name;
+	// String dob;
+	// String phoneNumber;
+	// String email;
+	// String ssn;
+	// String address;
+	// byte hasHotelCard;
 
-  private void createCustomer(String id, String name, String dob, String phoneNumber, String email, String ssn,  String address, String hasHotelCard)
-  {
-    try
-    {
-      //query
-    }
-    catch(Exception e)
-    {
-      System.out.println(e);
-    }
-  }
+	void createCustomer(String id, String name, String dob, String phoneNumber, String email, String ssn,
+			String address, String hasHotelCard) {
+		try {
+			// query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
-  private void updateCustomer(String id, String name, String dob, String phoneNumber, String email, String ssn,  String address, String hasHotelCard)
-  {
-    try
-    {
-      //query
-    }
-    catch(Exception e)
-    {
-      System.out.println(e);
-    }
-  }
+	void updateCustomer(String id, String name, String dob, String phoneNumber, String email, String ssn,
+			String address, String hasHotelCard) {
+		try {
+			// query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
-  private void deleteCustomer(String id)
-  {
-    try
-    {
-      //query
-    }
-    catch(Exception e)
-    {
-      System.out.println(e);
-    }
-  }
+	void deleteCustomer(String id) {
+		try {
+			// query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
-  private void assignRoom(int customerId, int hotelId ,int roomId)
-  {
-    try
-    {
-      //query
-    }
-    catch(Exception e)
-    {
-      System.out.println(e);
-    }
-  }
+	void assignRoom(int customerId, int hotelId, int roomId) {
+		try {
+			// query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
 }
