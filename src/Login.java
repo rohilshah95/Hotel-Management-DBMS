7c8226b83bd88255ac4caece7ea0eef5f61978f3
Uddhav Bhosle <ubhosle@ncsu.edu>
2018-04-13 19:50:55 -0400

    login

--- src/Login.java
+++ src/Login.java
@@ -1,11 +1,19 @@
 package src;
 
+import java.util.Scanner;
+
 public class Login {
-	int user=0;
-	
-	static int getUser()
-	{
-		System.out.println("Who is the user?:\n1. Admin\n2. Manager\n3. Front Desk Staff\n 4. Service Staff\n");
-		return 0;
+	int user = 0;
+
+	static int getUser() {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			System.out.println("Who is the user?:\n1. Admin\n2. Manager\n3. Front Desk Staff\n 4. Service Staff\n");
+			int user = sc.nextInt();
+			if (user < 1 || user > 4) {
+				System.out.println("Enter correct user...");
+			} else
+				return user;
+		}
 	}
 }
