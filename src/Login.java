f1e76f8e56b920078bd781a1e5248fab23a0ada3
rcoutin <rcoutin@ncsu.edu>
2018-04-14 02:41:33 -0400

    Fixing static variables

--- src/Login.java
+++ src/Login.java
@@ -3,9 +3,9 @@ package src;
 import java.util.Scanner;
 
 public class Login {
-	int user = 0;
+	static int user = 0;
 
-	int getUser() {
+	static int getUser() {
 		Scanner sc = new Scanner(System.in);
 		while (true) {
 			System.out.println("Login as:\n1. Admin\n2. Manager\n3. Front Desk Staff\n4. Service Staff\n");
