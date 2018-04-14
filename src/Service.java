af3291f87923eb576b820bcd131a2b5e66dd9d7c
Rohil Shah <rshah8@ncsu.edu>
2018-04-13 23:59:13 -0400

    crud of services

--- src/Service.java
+++ src/Service.java
@@ -1,41 +1,86 @@
 package src;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Service {
-	private void createService(String name, String cost) {
+	private void createService(int serviceId, String name, int cost) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
+		    PreparedStatement pstmt = conn.prepareStatement("INSERT INTO SERVICE(ID, NAME, COST) VALUES(?,?,?)");
+		    pstmt.setInt(1,  serviceId);
+		    pstmt.setString(2, name);
+		    pstmt.setInt(3, cost);
+		    
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 
-	private void deleteService(String id) {
+	private void deleteService(int id) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
+		    
+		    PreparedStatement pstmt=conn.prepareStatement("DELETE FROM SERVICE WHERE ID=?");
+		    pstmt.setInt(1, id);
+		    pstmt.executeUpdate();
+		    
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 
-	private void updateService(String id, String name, String cost) {
+	private void updateService(int serviceId, String name, int cost) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
+		    PreparedStatement pstmt = conn.prepareStatement("UPDATE SERVICE SET NAME=?, COST=? WHERE ID=?");
+		    pstmt.setString(1, name);
+		    pstmt.setInt(2, cost);
+		    pstmt.setInt(3, serviceId);
+		    
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
+	
+	public static ResultSet getService(int serviceId)
+	{
+		ResultSet rs = null;
+		try {
+			Connection conn = DBConnection.getConnection();
+		    Statement stmt = conn.createStatement();
+		    PreparedStatement pstmt = conn.prepareStatement("SELECT * from SERVICE WHERE ID=?");
+		    pstmt.setInt(1, serviceId);
+		    rs= pstmt.executeQuery();
 
+		    // query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return rs;
+	}
+	
+	public static ResultSet getAllServices()
+	{
+		ResultSet rs = null;
+		try {
+			Connection conn = DBConnection.getConnection();
+		    Statement stmt = conn.createStatement();
+	
+		    rs= stmt.executeQuery("SELECT * from SERVICE");
+			// query
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return rs;
+	}
+	
 }
