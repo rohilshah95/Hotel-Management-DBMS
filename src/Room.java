48182a86d69eac431642bd4260b9c91b44bf697e
Uddhav Bhosle <ubhosle@ncsu.edu>
2018-04-14 01:24:25 -0400

    room functions

--- src/Room.java
+++ src/Room.java
@@ -1,6 +1,7 @@
 package src;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -10,78 +11,104 @@ public class Room {
  * hotelID, number, maxOccupancy, nightlyRate, category, availaility;
  */
 	
-	void createRoom(String hotelID, String number, String category, String nightlyRate, String availability, String maxOccupancy) {
+	public static void createRoom(int id, int number, String category, int nightlyRate, int availability , int maxOccupancy) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
+		    PreparedStatement pstmt = conn.prepareStatement("INSERT INTO ROOM (HotelID, Number, Category, Rate, Availability, MaxOccupancy) VALUES (?,?,?,?,?,?)");
+		    pstmt.setInt(1, id);
+		    pstmt.setInt(2, number);
+		    pstmt.setString(3, category);
+		    pstmt.setInt(4, nightlyRate);
+		    pstmt.setInt(5, availability);
+		    pstmt.setInt(6, maxOccupancy);
+		    pstmt.executeUpdate();
+		    
 		    ResultSet rs = null;
-			// query
+//			// query
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	void editRoom(String hotelID, String number, String category, String nightlyRate, String availability, String maxOccupancy) {
+	
+	
+	
+	public static void updateRoom(int hotelID, int number, String category, int nightlyRate, int availability, int maxOccupancy) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
-			// query
+		    PreparedStatement pstmt = conn.prepareStatement("UPDATE ROOM SET category=?, rate=?, availability=?, maxOccupancy=? WHERE hotelid=? AND number=?");
+		    pstmt.setString(1, category);
+		    pstmt.setInt(2, nightlyRate);
+		    pstmt.setInt(3, availability);
+		    pstmt.setInt(4, maxOccupancy);
+		    pstmt.setInt(5, hotelID);
+		    pstmt.setInt(6, number);
+		    pstmt.executeUpdate();
+		    
+//		    stmt.executeUpdate("UPDATE CUSTOMER SET Name='"+name+"', dob='"+dob+"', phone='"+phoneNumber+"', email='"+email+"', ssn='"+ssn+"', address='"+address+"', hashotelcard="+hasHotelCard +"WHERE ID="+id+")");
+		    
+		    // query
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-	void deleteRoom(String hotelID, String number, String category, String nightlyRate, String availability, String maxOccupancy) {
+	
+	
+	
+	static void deleteRoom(String hotelID, int number) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
+		    
+		    PreparedStatement pstmt = conn.prepareStatement("DELETE FROM ROOM WHERE hotelId=? AND number=?");
+		    pstmt.setString(1, hotelID);
+		    pstmt.setInt(2, number);
+		    pstmt.executeUpdate();
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 	
-	void checkRoomAvailability() {
+	
+	static void checkRoomAvailability(int hotelId) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
 			// query
+		    stmt.executeUpdate("SELECT * FROM ROOM WHERE availability=1 AND hotelId="+hotelId);
+		    
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 	
-	void checkRoomAvailability(String category) {
+	static void checkRoomAvailability(int hotelId, String category) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
+		    stmt.executeUpdate("SELECT * FROM ROOM WHERE availability=1 AND hotelId="+hotelId + " AND category='"+category+"'");
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
+
 	
-	void assignRoom(String customerId, String hotelId, String roomNumber) {
+	static void releaseRoom(int hotelId, int number) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
-			// query
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		
-		// Create entry in check-in table
-		// Create a bill in bill table
-	}
-	
-	void releaseRoom(String hotelId, String number) {
-		try {
-			Connection conn = DBConnection.getConnection();
-		    Statement stmt = conn.createStatement();
-		    ResultSet rs = null;
+		    PreparedStatement pstmt = conn.prepareStatement("UPDATE ROOM SET availability=1 WHERE hotelid=? AND number=?");
+		    pstmt.setInt(1, hotelId);
+		    pstmt.setInt(2, number);
+		    pstmt.executeUpdate();
 			// query
 		} catch (Exception e) {
 			System.out.println(e);
@@ -89,24 +116,40 @@ public class Room {
 		// *********************************** MAKE CHANGES IN REPORT 1, TAKE HOTEL ID AS A PARAMETER IN THE FUNCTION **************************
 	}
 	
-	void addServiceToRoom(String hotelId, String number, String serviceId) {
+	static void addServiceToRoom(int hotelId, int number, int staffId,int  serviceId) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
 			// query
+		    PreparedStatement pstmt = conn.prepareStatement("INSERT INTO PROVIDES (HotelID, Number, StaffID, ServiceID, Timestamp) VALUES (?,?,?,?,NOW())");
+		    pstmt.setInt(1, hotelId);
+		    pstmt.setInt(2, number);
+		    pstmt.setInt(3, staffId);
+		    pstmt.setInt(4, serviceId);
+		    pstmt.executeUpdate();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 		// *********************************** MAKE CHANGES IN REPORT 1, TAKE HOTEL ID AS A PARAMETER IN THE FUNCTION **************************
 	}
 	
-	void addStaffToPresidential(String hotelId, String number) {
+	static void addStaffToPresidential(int hotelId, int number, int staffId) {
 		try {
 			Connection conn = DBConnection.getConnection();
 		    Statement stmt = conn.createStatement();
 		    ResultSet rs = null;
 			// query
+		    PreparedStatement pstmt = conn.prepareStatement("INSERT INTO ASSIGNED (HotelID, Number, StaffID) VALUES (?,?,?)");
+		    pstmt.setInt(1, hotelId);
+		    pstmt.setInt(2, number);
+		    pstmt.setInt(3, staffId);
+		    pstmt.executeUpdate();
+		    PreparedStatement pstmt2 = conn.prepareStatement("UPDATE STAFF SET availability=0 WHERE hotelid=? AND staffId=?");
+		    pstmt2.setInt(1, hotelId);
+		    pstmt2.setInt(2, staffId);
+		    pstmt2.executeUpdate();
+		    
 		} catch (Exception e) {
 			System.out.println(e);
 		}
