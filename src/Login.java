116d7390956af9b1cfad6c201c24a50c6c723d6c
rcoutin <rcoutin@ncsu.edu>
2018-04-15 05:54:10 -0400

    Adding permissions to information processing

--- src/Login.java
+++ src/Login.java
@@ -5,15 +5,21 @@ public class Login {
 
 	static int getUser() {
 		while (true) {
-			System.out.println("Login as:\n1. Admin\n2. Manager\n3. Front Desk Staff\n4. Service Staff\n");
+			System.out.println("Login as:\n1. Admin\n2. Manager\n3. Front Desk Staff\n4. Service Staff\n5. Exit");
 			int user = Integer.parseInt(TeamT.readInput());
-			return user;
+			if (user < 1 || user > 5) {
+				System.out.println("Enter valid input");
+			} else if (user == 5) {
+				System.exit(0);
+			} else {
+				return user;
+			}
 		}
 	}
 
 	public static int getHotelID() {
 		System.out.print("Enter HotelID: ");
-		int hotelID = Integer.parseInt(TeamT.readInput());
+		int hotelID = TeamT.readInt();
 		return hotelID;
 	}
 }
